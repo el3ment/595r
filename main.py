436967ac101f72e11a6472d53704749e34bbd3a9
Robert Pottorff <rpottorff@gmail.com>
2017-10-17 16:13:44 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,138 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+import scipy.io as sio
+
+# large plots for ipython notebook
+matplotlib.rcParams['figure.figsize'] = (17.0, 17.0)
+
+
+class Estimator:
+    def __init__(self, initial, landmarks):
+        self.x_hat = initial
+        self.P = np.eye(self.x_hat.shape[0]) * 0.1
+        self.landmarks = landmarks
+
+    def propagate(self, dt, u):
+        self.x_hat = self.dynamics(dt, self.x_hat, u)
+
+        # TODO: implement propagation step here
+
+        return self.x_hat, self.P
+
+    def update(self, dt, z):
+        zhat = self.measure(dt, self.x_hat)
+
+        # compute the difference betwee then predicted and measured 22 landmark measurements
+        # (i.e range and bearing for 11 landmarks)
+        # then remove all the nans, only leaving the landmarks we can see
+        # additionally, visible_landmarks contains a vector of visible landmark indexes
+        visible_landmarks = np.where(~np.isnan(z[::2, 0]))[0]
+        zdiff = zhat - z
+        zdiff = zdiff[~np.isnan(zdiff)]
+
+        # TODO: implement update step here
+
+        return self.x_hat, self.P
+
+    def measure(self, dt, x, Q=None):
+        # prevent anyone from making a mistake here
+        assert x.shape == (3, 1) and (
+        Q is None or Q.shape == (3, 3)), "bad shapes for measurement: {} should be (3,1) and {} should be (3,3)".format(
+            u.shape, x.shape)
+
+        z = np.zeros([2 * self.landmarks.shape[0]])
+
+        for i, m in enumerate(self.landmarks):
+            q = (m[0] - x[0]) ** 2 + (m[1] - x[1]) ** 2
+            z[i * 2] = np.sqrt(q)
+            z[i * 2 + 1] = np.arctan2(m[1] - x[1], m[0] - x[0]) - x[2]
+
+        z = np.random.multivariate_normal(z, Q) if Q else z
+
+        return z[:, None]
+
+    def dynamics(self, dt, x, u):
+        # prevent anyone from making a mistake here
+        assert u.shape == (2, 1) and x.shape == (
+        3, 1), "bad shapes for dynamics: {} should be (2,1) and {} should be (3,1)".format(u.shape, x.shape)
+
+        noise_v, noise_omega = 0, 0
+        v, omega = u[0] + noise_v, u[1] + noise_omega
+
+        if omega == 0:
+            omega = 0.00000001
+
+        px, py, theta = x[0], x[1], x[2]
+        px += v / omega * np.sin(theta + omega * dt) - v / omega * np.sin(theta)
+        py += v / omega * np.cos(theta) - v / omega * np.cos(theta + omega * dt)
+        theta += omega * dt
+
+        return np.array([px, py, theta])
+
+
+matfile = sio.loadmat('processed_data.mat')
+landmark_bearing = matfile['l_bearing'].T
+landmark_range = matfile['l_depth'].T
+landmark_t = matfile['l_time'][:, 0]
+landmarks = matfile['landmarks']
+odometry_t = matfile['odom_t'][0]
+odometry_vel = matfile['vel_odom'].T
+odometry_pos = matfile['pos_odom_se2'].T
+x_hat = odometry_pos[0, None].T  # (3, 1)
+
+estimator = Estimator(initial=x_hat, landmarks=landmarks)
+measurement_index = 0
+x_history = []
+p_history = []
+
+for i, (t, dt) in enumerate(zip(odometry_t, np.diff(np.concatenate([[0], odometry_t])))):
+    u = odometry_vel[i, None].T  # (2, 1)
+
+    # if we have stepped over a landmark measurement, and there exists a measurement
+    if measurement_index < landmark_t.shape[0] and t > landmark_t[measurement_index]:
+        # compute the time since the last propagate and this measurement update
+        fractional_dt = landmark_t[measurement_index] - (t - dt)
+
+        # build the measurement, [b_0, r_0, b_1, r_1, ... , b_n, r_n]
+        z = np.ravel(np.column_stack([landmark_range[measurement_index],
+                                      landmark_bearing[measurement_index]]))[:, None]
+
+        # this propagate step will be the last odometry to the landmark sensor measurement
+        x_hat, P = estimator.propagate(fractional_dt, u)
+
+        # update the model
+        x_hat, P = estimator.update(fractional_dt, z)
+
+        # the next propagate step will be the time from after the measurement
+        # to the next odometry message
+        dt = t - landmark_t[measurement_index]
+
+        measurement_index += 1
+
+    x_hat, P = estimator.propagate(dt, u)
+
+    x_history.append(x_hat[:, 0])
+    p_history.append(P)
+
+x_history = np.array(x_history)
+p_history = np.array(p_history)
+
+# plot the x,y,theta
+# and confidence intervals
+plt.subplot(2, 1, 1)
+[plt.plot(odometry_t, y.T, label=l) for y, l in zip(odometry_pos.T, ['x_odometry', 'y_odometry', 'theta_odometry'])]
+[plt.plot(odometry_t, y.T, label=l) for y, l in zip(x_history.T, ['x_hat', 'y_hat', 'theta_hat'])]
+[plt.plot(odometry_t, x_history[:, i] + 3.0 * p_history[:, i, i] ** 0.5, '-k', alpha=0.5) for i in range(3)]
+[plt.plot(odometry_t, x_history[:, i] - 3.0 * p_history[:, i, i] ** 0.5, '-k', alpha=0.5) for i in range(3)]
+plt.legend()
+
+# plot the global position and landmarks
+plt.subplot(2, 1, 2)
+plt.plot(odometry_pos.T[0], odometry_pos.T[1], label='odometry_position')
+plt.plot(x_history.T[0], x_history.T[1], label='estimated_position')
+plt.plot(landmarks.T[0], landmarks.T[1], 'o', label='landmarks')
+plt.legend()
+plt.show()
+
